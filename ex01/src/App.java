1e2f376cef657ff2710dc2232313d7c391ecc41e
cicero <ciceroprr12@gmail.com>
2023-07-17 15:56:28 -0300

    novo hi

--- ex01/src/App.java
+++ ex01/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hii!");
+        System.out.println("Este é um novo Hii!");
     }
 }
